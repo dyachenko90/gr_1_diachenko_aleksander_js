9781156cb6bca9ee32bec1f4b011475e79b2ce79
dyachenko90 <90046044+dyachenko90@users.noreply.github.com>
2021-10-28 16:24:56 +0300

    Update homework_2.js

--- homework_2.js
+++ homework_2.js
@@ -1,16 +1,19 @@
-// let numeric = prompt ('Введите число');
-// numeric = Number(numeric);
-// input_first = numeric%10;
-// input_second = numeric%100;
-// if (input_second == 11 || input_second == 12 || input_second == 13 || input_second == 14) {
-//     alert(`${numeric} дней`);
-// } else if (input_first == 1) {
-//     alert(`${numeric} день`);
-// } else if (input_first == 2 || input_first == 3 || input_first == 4) {
-//     alert(`${numeric} дня`);
-// } else {
-//     alert (`${numeric} дней`);
+// Задача №1
+let numeric = prompt('Введите число');
+numeric = Number(numeric);
+input_first = numeric%10;
+input_second = numeric%100;
+if (input_second == 11 || input_second == 12 || input_second == 13 || input_second == 14) {
+    alert(`${numeric} дней`);
+} else if (input_first == 1) {
+    alert(`${numeric} день`);
+} else if (input_first == 2 || input_first == 3 || input_first == 4) {
+    alert(`${numeric} дня`);
+} else {
+    alert (`${numeric} дней`);
+}
 
+// Задача №2
 let n = prompt('Введите расстояние (км)');
 let d = prompt('Введите размер колеса (дюйм)');
 const PI = 3.14;
