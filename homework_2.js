0396e5c06e78d84605ac69a2da3b28a36b268120
dyachenko90 <90046044+dyachenko90@users.noreply.github.com>
2021-11-01 10:39:17 +0200

    Update homework_2.js

--- homework_2.js
+++ homework_2.js
@@ -1,33 +1,43 @@
 // Задача №1
 let numeric = prompt('Введите число');
 numeric = Number(numeric);
-input_first = numeric%10;
-input_second = numeric%100;
-if (input_second == 11 || input_second == 12 || input_second == 13 || input_second == 14) {
+
+input_first = numeric % 10;
+input_second = numeric % 100;
+
+if (numeric > 10 && numeric < 20) {
     alert(`${numeric} дней`);
-} else if (input_first == 1) {
+} else if (input_first === 1) {
     alert(`${numeric} день`);
-} else if (input_first == 2 || input_first == 3 || input_first == 4) {
+} else if (input_first === 2 || input_first === 3 || input_first === 4) {
     alert(`${numeric} дня`);
 } else {
     alert (`${numeric} дней`);
 }
 
 // Задача №2
-let n = prompt('Введите расстояние (км)');
-let d = prompt('Введите размер колеса (дюйм)');
+let N = prompt('Введите расстояние (км)');
+let D = prompt('Введите размер колеса (дюйм)');
+
+N = Number(N);
+D = Number(D);
+
+const МETERS_IN_KILOMETERS = 1000;
+const METERS_IN_INCH = 0.0254;
 const PI = 3.14;
-const D = 2.54;
-n = Number(n);
-d = Number(d);
-if (n==Number(n) || d==Number(d)) {
-    console.log('Данные введены правильно');
+
+if (isNaN(N) || isNaN(D)) {
+    console.log('Данные введены неверно');
 } else {
-    console.log('Введите корректные данные');
+    N = N * МETERS_IN_KILOMETERS;
+    D = D * METERS_IN_INCH;
+
+    let result_one_turn = N / (D * PI);
+    let result_four_turn = 4 * result_one_turn;
+
+    alert(`Количество оборотов одного колеса: ${Math.trunc(result_one_turn)}`);
+    alert(`Сумма оборотов всех колес: ${Math.trunc(result_four_turn)}`);
 }
-result = Math.round(n/((d*PI*D/10**5)));
-alert(`Количество оборотов одного колеса: ${result}`);
-alert(`Сумма оборотов всех колес: ${result*4}`);
 
 
 
