fe49b41cd3afe76f0cf02cb1b4be08f7d8c34925
dyachenko90 <90046044+dyachenko90@users.noreply.github.com>
2021-11-09 08:47:54 +0200

    Update homework_6.js

--- homework_6.js
+++ homework_6.js
@@ -1,5 +1,3 @@
-
-
 // Напишите функцию get_count_work_days(date1, date2), которая будет считать количество рабочих дней между двумя заданными датами.
 
 // const MS_SECOND = 1000;
@@ -7,28 +5,39 @@
 // const MS_HOUR = MS_MINUTE * 60;
 // const MS_DAY = MS_HOUR * 24;
 
+
 // function get_count_work_days(date1, date2) {
 //     if (date1 instanceof Date && date2 instanceof Date) {
-//         let ms_result = date1.getTime() - date2.getTime();
-//         let result = Math.abs(ms_result / MS_DAY);
-//         return result;
-//     }
-//     return null;
-// }
-// let a = get_count_work_days(new Date(2021, 6, 1), new Date(2021, 6, 22));
-// console.log(a)
-//     input_first = a%10;
-//     input_second = a%100;
-//     if (input_second > 10 && input_second < 20) {
-//         console.log(`Разница дней между двумя датами составляет ${a} дней`);
-//     } else if (input_first === 1) {
-//         console.log(`Разница дней между двумя датами составляет ${a} день`);
-//     } else if (input_first === 2 || input_first === 3 || input_first === 4) {
-//         console.log(`Разница дней между двумя датами составляет ${a} дня`);
-//     } else {
-//         console.log(`Разница дней между двумя датами составляет ${a} дней`);
-//     }
+//       let result = 0;
 
+//       let ms_start = date1.getTime();
+//       let ms_finish = date2.getTime();
+
+//       for (let i = ms_start; i < ms_finish; i += MS_DAY) {
+//         let iterration_date = new Date(i);
+//         let index_day_of_week = iterration_date.getDay();
+//         if (index_day_of_week >= 1 && index_day_of_week <= 5) {
+//           result += 1;
+//         }
+//     }
+//     return result;
+//   }
+//   return null;
+// }
+//     let d1 = new Date();
+//     let d2 = new Date(2021, 11, 31);
+//     let work_day = get_count_work_days(d1, d2);
+//     let input_first = work_day%10;
+//     let input_second = work_day%100;
+//     if (input_second > 10 && input_second < 20) {
+//         console.log(`Разница дней между двумя датами составляет ${work_day} дней`);
+//     } else if (input_first === 1) {
+//         console.log(`Разница дней между двумя датами составляет ${work_day} день`);
+//     } else if (input_first === 2 || input_first === 3 || input_first === 4) {
+//         console.log(`Разница дней между двумя датами составляет ${work_day} дня`);
+//     } else {
+//         console.log(`Разница дней между двумя датами составляет ${work_day} дней`);
+//     }
   
 // Напишите функцию get_count_days(date) , которая будет считать количество дней до Нового года от date, 
 // если оно задано, иначе посчитать количество дней от текущей даты
