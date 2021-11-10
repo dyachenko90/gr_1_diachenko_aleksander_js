c775eb501a6534ebb8b53bedaa11026a7bf3893a
dyachenko90 <90046044+dyachenko90@users.noreply.github.com>
2021-11-10 14:35:37 +0200

    Update homework_7.js

--- homework_7.js
+++ homework_7.js
@@ -73,3 +73,57 @@
 // Например после действия сумма в истории должна появится такая запись: 
 // "Мой калькулятор (8.11.2021 20:30): сумма = 10, (5, 5)"
 // Калькулятор должен уметь вывести историю действий в консоль и уметь очищать историю действий.
+
+
+// function Calculator (name) {
+//     this.name = name;
+// }
+// let calculator = new Calculator('Мой калькулятор');
+
+//     calculator.add = function() {
+//         if (isNaN(arguments)) {
+//             let result = 0;
+//             for (let i = 0; i < arguments.length; i += 1) {
+//                 result += arguments[i];
+//             }
+//             return result;
+//         }
+//         return null;
+//     }
+ 
+//     calculator.division = function(a, b) {
+//         if (!isNaN(a) || !isNaN(b)) {
+//             let result = 0;
+//             result = a / b;
+//             return result;
+//         }
+//         return null;
+//     }
+
+//     calculator.subtraction = function(a, b) {
+//         if (!isNaN(a) || !isNaN(b)) {
+//             let result = 0;
+//             result = a - b;
+//             return result;
+//             } 
+//         return null;
+//     }
+
+//     calculator.multiply = function () {
+//         if (isNaN(arguments)) {
+//             let result = 1;
+//             for (let i = 0; i < arguments.length; i += 1) {
+//                 result *= arguments[i];
+//             }
+//             return result;
+//         }
+//         return null;
+//     }
+//     let date = new Date();
+//     let today = date.getMonth() + 1;
+//     let dateTimeToday = `(${date.getDate()}.${today}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}): `;
+    
+//     console.log(`${calculator.name} ${dateTimeToday} результат деления: ${calculator.division(4, 2)}`);
+//     console.log(`${calculator.name} ${dateTimeToday} результат вычитания: ${calculator.subtraction(5, 2)}`);
+//     console.log(`${calculator.name} ${dateTimeToday} результат сложения: ${calculator.add(6, 8)}`);
+//     console.log(`${calculator.name} ${dateTimeToday} результат умножения: ${calculator.multiply(3, 5, 6, 9)}`);
